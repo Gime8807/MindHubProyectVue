7d72757a12be9994cef875a4dd4c25e322604292
gimena sasso <94083786+Gime8807@users.noreply.github.com>
2023-03-21 23:51:20 -0300

    primer commit, creacion de html, assets y primeras lineas en js

---
+++ assets/js/index.js
@@ -0,0 +1,29 @@
+const {createApp} = Vue
+const url = "https://mindhub-xj03.onrender.com/api/amazing"
+
+createApp ({
+    data(){
+        return{
+            arrayData:[],
+            categorys:[]
+        }
+    },
+    created(){
+        fetch(url)
+        .then (response =>response.json())
+        .then(data=>{
+            this.arrayData = data
+            console.log(this.arrayData)
+            this.categorys = [...new Set(this.arrayData.events.map(function (events){return events.category}))]
+            console.log(this.categorys)
+        })
+        .catch(error =>console.log(error))
+
+    },
+    methods:{
+
+    },
+    computed:{
+
+    }
+}).mount ('#app')
