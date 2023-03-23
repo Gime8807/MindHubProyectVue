4332ec857d3e094f876c6d889b6f690357c4c06c
gimena sasso <94083786+Gime8807@users.noreply.github.com>
2023-03-23 00:10:45 -0300

    index funcionando, past y uncoming

--- assets/js/uncomig.js
+++ assets/js/uncomig.js
@@ -15,18 +15,16 @@ createApp ({
         fetch(url)
         .then (response =>response.json())
         .then(data=>{
-            this.arrayData = data
-            console.log(this.arrayData)
-            this.arrayFiltered = this.arrayData.events
-            this.categorys = [...new Set(this.arrayData.events.map(function (events){return events.category}))]
-            console.log(this.categorys)
+            this.arrayData = data.events.filter(evento => Date.parse(evento.date) > Date.parse(data.currentDate))
+            this.arrayFiltered = this.arrayData
+            this.categorys = [...new Set(data.events.map(function (events){return events.category}))]
         })
         .catch(error =>console.log(error))
 
     },
     methods:{
         filter(){
-            this.arrayFiltered= this.arrayData.events.filter(evento =>{
+            this.arrayFiltered= this.arrayData.filter(evento =>{
                 return (this.checked.includes(evento.category)|| this.checked.length ===0 ) && evento.name.toLowerCase().includes(this.inputSearch.toLowerCase())
             })
 
